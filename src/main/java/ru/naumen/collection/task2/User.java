690d23c475e5f38f8389d4a0e2dda1feae2f3b53
Almazova Vlada <almazova.2004@mail.ru>
2024-10-04 13:02:30 +0500

    решение задачи 2

--- src/main/java/ru/naumen/collection/task2/User.java
+++ src/main/java/ru/naumen/collection/task2/User.java
@@ -1,5 +1,7 @@
 package ru.naumen.collection.task2;
 
+import java.util.Arrays;
+
 /**
  * Пользователь
  *
@@ -10,4 +12,21 @@ public class User {
     private String username;
     private String email;
     private byte[] passwordHash;
+
+    //переопределение методов чтобы можно было сравнивать экземпляры класса и хранить их по хэшу
+    @Override
+    public int hashCode() {
+        return username.hashCode() + email.hashCode() + Arrays.hashCode(passwordHash);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == this)
+            return true;
+        if(obj instanceof User){
+            User that = (User) obj;
+            return username.equals(that.username) && email.equals(that.email) && Arrays.equals(passwordHash, that.passwordHash);
+        }
+        return false;
+    }
 }
